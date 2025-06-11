b948e1a238d82b7fa70f5535d225088c72194a8c
Jrobinson718 <jrobinson6284@gmail.com>
2025-06-11 11:32:35 -0400

    Feat: Main menu complete

--- NorthwindManager/src/main/java/com/pluralsight/Main.java
+++ NorthwindManager/src/main/java/com/pluralsight/Main.java
@@ -5,5 +5,6 @@ import com.pluralsight.UI.UserInterface;
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
+        ui.display();
     }
 }
